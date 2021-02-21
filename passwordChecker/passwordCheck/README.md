084771662b3120686a909ba3e26a338bb67dab9d
Navid <navidkaen@gmail.com>
2021-02-21 11:23:18 +0330

    password checker added

---
+++ passwordChecker/passwordCheck/README.md
@@ -0,0 +1,7 @@
+# passwordCheck
+This is a script for checking that how many times your passwords used.
+
+1.Run passCheck_1 with your password.example: 
+python3 passCheck_1.py 'your password'
+2.Run passCheck_2 without any argument.
+
---
+++ passwordChecker/passwordCheck/passCheck_2.py
@@ -0,0 +1,43 @@
+import requests
+import hashlib
+import sys
+
+
+# send request to api
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error: {res.status_code}, check the api')
+    return res
+
+
+# split passwords which returned from api
+def seperate_password(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+# convert password to hash code
+def pwned_check(password):
+    # check the password if exist or not
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return seperate_password(response, tail)
+
+
+# give passwords from user and send it to converter func
+def main(password):
+    count = pwned_check(password)
+    if count:
+        print(f'{password} was found {count} times...')
+    else:
+        print(f'{password} was NOT found. It is Safe')
+
+
+if __name__ == '__main__':
+    sys.exit(main(input('enter your passwords: ')))
